61eac6936fc85dc0be5da933819456b25777c01a
atrobman <atrobman@vt.edu>
2020-05-20 16:07:04 -0400

    Beginning of OCR implementation

--- ocr.py
+++ ocr.py
@@ -4,12 +4,13 @@ import cv2
 
 class OCR:
 	
-	def __init__(self, east, min_confidence=0.5):
+	def __init__(self, min_confidence=0.5):
 
-		self.east = east
+		self.net = cv2.dnn.readNet("./frozen_east_test_detection.pb")
 		self.min_confidence = min_confidence
 		self.image = None
-	
+		self.rescale = (0,0)
+
 	def read_image(self, inp):
 
 		img = cv2.imread(inp)
@@ -18,8 +19,6 @@ class OCR:
 		M, N = img.shape[:2]
 		rM = M / 320
 		rN = N / 320
+		self.rescale = (rM, rN)
 
-		img = cv2.resize(img, (320, 320))
-
-		cv2.imshow('test', img)
-		cv2.imshow('orig', orig)
+		self.img = cv2.resize(img, (320, 320))
